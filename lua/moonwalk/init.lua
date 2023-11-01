476dd84ccd315d56b9054af488cb64fa901d1f1a
Jiàn <thejianmail@gmail.com>
2023-11-01 22:55:59 +0800

    Init proj

---
+++ lua/moonwalk/init.lua
@@ -0,0 +1,9 @@
+local M = {}
+
+function M.load()
+	if vim.g.colors_name then vim.cmd.highlight "clear" end
+	if vim.fn.exists "syntax_on" then vim.cmd.syntax "reset" end
+	vim.o.termguicolors = true
+	vim.g.colors_name = "moonwalk"
+end
+
