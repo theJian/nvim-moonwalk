5c146f60ffa2eff9f7b5569e9287ce97c0b46cc5
Jiàn <thejianmail@gmail.com>
2023-11-02 21:34:29 +0800

    Live reload theme

--- lua/moonwalk/init.lua
+++ lua/moonwalk/init.lua
@@ -4,11 +4,12 @@ local util = require 'moonwalk.util'
 local M = {}
 
 function M.load()
-	if vim.g.colors_name then vim.cmd.highlight "clear" end
-	if vim.fn.exists "syntax_on" then vim.cmd.syntax "reset" end
+	if vim.g.colors_name then vim.cmd.highlight 'clear' end
+	if vim.fn.exists 'syntax_on' then vim.cmd.syntax 'reset' end
 	vim.o.termguicolors = true
-	vim.g.colors_name = "moonwalk"
+	vim.g.colors_name = 'moonwalk'
 	util.load_schemes()
+	if vim.env.MOONWALK_DEV then util.live_reloading() end
 end
 
 return M
--- lua/moonwalk/util.lua
+++ lua/moonwalk/util.lua
@@ -1,13 +1,19 @@
-local palette = require 'moonwalk.palette'
-
 local M = {}
+
+function M.get_palette()
+	if vim.env.MOONWALK_DEV then package.loaded['moonwalk.palette'] = nil end
+	local palette = require 'moonwalk.palette'
+	return palette
+end
 
 function M.load_schemes()
 	local names = {
 		'base',
 	}
+	local palette = M.get_palette()
 	for _, name in ipairs(names) do
 		local module = 'moonwalk.schemes.' .. name
+		if vim.env.MOONWALK_DEV then package.loaded[module] = nil end
 		local f = require(module)
 		local scheme = f({ colors = palette.default })
 		for group, hl in pairs(scheme) do
@@ -18,6 +24,21 @@ end
 
 function M.highlight(group, hl)
 	vim.api.nvim_set_hl(0, group, hl)
+end
+
+function M.live_reloading()
+	if vim.env.MOONWALK_DEV then
+		vim.api.nvim_create_autocmd('BufWritePost', {
+			group = vim.api.nvim_create_augroup('NvimMoonwalk', { clear = true }),
+			pattern = '*.lua',
+			callback = function()
+				local theme = vim.g.colors_name
+				if string.match(theme, 'moonwalk') then
+					vim.cmd("colorscheme moonwalk")
+				end
+			end,
+		})
+	end
 end
 
 return M
