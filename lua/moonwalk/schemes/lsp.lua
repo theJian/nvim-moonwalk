206d49ba2ead733cbddff4b3bd28f132614e68ee
Jiàn <thejianmail@gmail.com>
2023-12-06 15:24:45 +0400

    add more lsp highlights

--- lua/moonwalk/schemes/lsp.lua
+++ lua/moonwalk/schemes/lsp.lua
@@ -10,8 +10,26 @@ local function treesitter(opts, c)
     LspCodeLensSeparator = { fg = c.red_soft },
 
     LspSignatureActiveParameter = { underline = true, bg = c.green_bg },
+    LspInlayHint = { bg = c.fg_softest, fg = c.fg_bold },
+    SnippetTabstop = { link = 'Visual' },
 
     -- Semantic Highlights
+    ['@lsp.type.class'] = { link = 'Structure' },
+    ['@lsp.type.comment'] = { link = 'Comment' },
+    ['@lsp.type.decorator'] = { link = 'Function' },
+    ['@lsp.type.enum'] = { link = 'Structure' },
+    ['@lsp.type.enumMember'] = { link = 'Constant' },
+    ['@lsp.type.function'] = { link = 'Function' },
+    ['@lsp.type.interface'] = { link = 'Structure' },
+    ['@lsp.type.macro'] = { link = 'Macro' },
+    ['@lsp.type.method'] = { link = 'Function' },
+    ['@lsp.type.namespace'] = { link = 'Structure' },
+    ['@lsp.type.parameter'] = { link = 'Identifier' },
+    ['@lsp.type.property'] = { link = 'Identifier' },
+    ['@lsp.type.struct'] = { link = 'Structure' },
+    ['@lsp.type.type'] = { link = 'Type' },
+    ['@lsp.type.typeParameter'] = { link = 'TypeDef' },
+    ['@lsp.type.variable'] = { link = 'Identifier' },
     ['@lsp.typemod.class.declaration'] = { fg = c.purple_bold, bold = true },
     ['@lsp.mod.deprecated'] = { strikethrough = true },
 
