e605bd99943e0695f8507b9ce4109a3b872ee079
Jiàn <thejianmail@gmail.com>
2025-02-20 00:22:05 +0800

    fix terminal color init

--- lua/moonwalk/util.lua
+++ lua/moonwalk/util.lua
@@ -47,29 +47,29 @@ end
 function M.set_terminal_color(flavour)
   local c = require('moonwalk.palette').get_colors(flavour, 'term')
 
-  vim.g.terminal_color_0 = c.term.black
-  vim.g.terminal_color_8 = c.term.bright_black
+  vim.g.terminal_color_0 = c.black
+  vim.g.terminal_color_8 = c.bright_black
 
-  vim.g.terminal_color_1 = c.term.red
-  vim.g.terminal_color_9 = c.term.bright_red
+  vim.g.terminal_color_1 = c.red
+  vim.g.terminal_color_9 = c.bright_red
 
-  vim.g.terminal_color_2 = c.term.green
-  vim.g.terminal_color_10 = c.term.bright_green
+  vim.g.terminal_color_2 = c.green
+  vim.g.terminal_color_10 = c.bright_green
 
-  vim.g.terminal_color_3 = c.term.yellow
-  vim.g.terminal_color_11 = c.term.bright_yellow
+  vim.g.terminal_color_3 = c.yellow
+  vim.g.terminal_color_11 = c.bright_yellow
 
-  vim.g.terminal_color_4 = c.term.blue
-  vim.g.terminal_color_12 = c.term.bright_blue
+  vim.g.terminal_color_4 = c.blue
+  vim.g.terminal_color_12 = c.bright_blue
 
-  vim.g.terminal_color_5 = c.term.purple
-  vim.g.terminal_color_13 = c.term.bright_purple
+  vim.g.terminal_color_5 = c.purple
+  vim.g.terminal_color_13 = c.bright_purple
 
-  vim.g.terminal_color_6 = c.term.cyan
-  vim.g.terminal_color_14 = c.term.bright_cyan
+  vim.g.terminal_color_6 = c.cyan
+  vim.g.terminal_color_14 = c.bright_cyan
 
-  vim.g.terminal_color_7 = c.term.white
-  vim.g.terminal_color_15 = c.term.bright_white
+  vim.g.terminal_color_7 = c.white
+  vim.g.terminal_color_15 = c.bright_white
 end
 
 return M
